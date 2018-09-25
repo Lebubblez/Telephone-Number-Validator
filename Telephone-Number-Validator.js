9a99166e48e55edf5595b4e802e0445e8bcd7faa
Leda Erlandson <leda.erlandson@gmail.com>
2018-09-25 07:14:14 -1000

    Second (big edit)
    changed to using regex

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -1,6 +1,6 @@
 function telephoneCheck(str) {
   // Good luck!
-  var por1 = /(\d|\))/g
+  var por1 = /(\d{1}|\()/g
   var space = /(\s)/g
   var spacedash = /(\s|\-)/g
   var par = /(\))/g
@@ -8,15 +8,17 @@ function telephoneCheck(str) {
 
 
   if(str.charCodeAt(0) === por1
-  && str.charCode(1,5) === space
-  && str.charCode(3,7,9) === spacedash
-  && str.charcode(4) === par
-  && str.charCode(8) === dash)) {
-  true;
+  && str.charCodeAt(1,5) === space
+  && str.charCodeAt(3,7,9) === spacedash
+  && str.charcodeAt(4) === par
+  && str.charCodeAt((8) === dash)) {
+  return true;
 }
 
+
+
  else {
-  false;
+  return false;
 }
 
 }
