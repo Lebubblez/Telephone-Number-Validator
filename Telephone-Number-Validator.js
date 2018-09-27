d5ef2d68df3b86776783480f2106b9367211c727
Leda Erlandson <leda.erlandson@gmail.com>
2018-09-26 17:16:52 -1000

    Edit 6: regex change, 2 fails still
    changed from /W to more specific characters

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -2,7 +2,7 @@ function telephoneCheck(str) {
   //check to start with 1 or /W (any nonword, number or underscore)
   //make an or (|) statement if it starts with parenthesis, so it must end with parenthesis after 3 digits
   //if no parenthesis, just the 3 digits part of or statment will continue
-  var regex = /^(1\W)?(\(\d{3}\)|\d{3})\W?\d{3}\W?\d{4}$/;
+  var regex = /^1?\s?(\d{3}|\(\d{3}\))(\s?|\-?)?\d{3}(-|\s)?\d{4}?/;
 if(str.match(regex))
 {
   return true;
