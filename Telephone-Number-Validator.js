73dad6bdf53f1a129f553f361a4ce1a1973868e4
Leda Erlandson <leda.erlandson@gmail.com>
2018-09-26 16:57:16 -1000

    Edit 5: Added comments
    Simplified regex and am left with one failed test

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -1,12 +1,14 @@
 function telephoneCheck(str) {
-	//
-	var regex = /^[1\(]?[-. ]?[\(]?([0-9]{3})([\)]?|[-.]?)[ ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  //check to start with 1 or /W (any nonword, number or underscore)
+  //make an or (|) statement if it starts with parenthesis, so it must end with parenthesis after 3 digits
+  //if no parenthesis, just the 3 digits part of or statment will continue
+  var regex = /^(1\W)?(\(\d{3}\)|\d{3})\W?\d{3}\W?\d{4}$/;
 if(str.match(regex))
 {
-	return true;
+  return true;
 }
 else {
-	return false;
+  return false;
 }
 }
-console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("1 555)555-5555"));
