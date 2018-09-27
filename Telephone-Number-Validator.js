6fcd628dae5bb82f29cb545e532f3d1043efaa5c
Leda Erlandson <leda.erlandson@gmail.com>
2018-09-26 14:31:31 -1000

    Edit 2: Regex added
    Added Regex and If statement

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -1,26 +1,12 @@
 function telephoneCheck(str) {
-  // Good luck!
-  var por1 = /(\d{1}|\()/g
-  var space = /(\s)/g
-  var spacedash = /(\s|\-)/g
-  var par = /(\))/g
-  var dash = /(\-)/g
-
-
-  if(str.charCodeAt(0) === por1
-  && str.charCodeAt(1,5) === space
-  && str.charCodeAt(3,7,9) === spacedash
-  && str.charcodeAt(4) === par
-  && str.charCodeAt((8) === dash)) {
+  //
+  var regex = /^[\(1]? [-. ]? ([0-9]{3}) \)? [-. ]? ([0-9]{3}) [-. ]? ([0-9]{4})$/;
+if((str.value.match(regex))
+{
   return true;
 }
-
-
-
- else {
+else {
+  alert("Type a Real Phone Number");
   return false;
 }
-
-}
-
 console.log(telephoneCheck("555-555-5555"));
